b2f33943fa0b55ef4f7449437a371dfb2a00b0fd
Thiago Cedran Santos <thiagocdn@gmail.com>
2025-02-09 21:30:29 -0300

    Cleaned examples test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,29 +3,3 @@ const ganache = require("ganache");
 const { Web3 } = require("web3");
 
 const web3 = new Web3(ganache.provider());
-
-class Car {
-  park() {
-    return "stopped";
-  }
-
-  drive() {
-    return "vroom";
-  }
-}
-
-let car;
-
-beforeEach(() => {
-  car = new Car();
-});
-
-describe("Car", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
-
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
-  });
-});
