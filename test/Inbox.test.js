cc9737004b1f8a5df3233b88f6288e1f65c39c9b
Thiago Cedran Santos <thiagocdn@gmail.com>
2025-02-09 21:30:00 -0300

    Added test example

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,31 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 
-// updated ganache and web3 imports added for convenience
+const web3 = new Web3(ganache.provider());
 
-// contract test code will go here
+class Car {
+  park() {
+    return "stopped";
+  }
+
+  drive() {
+    return "vroom";
+  }
+}
+
+let car;
+
+beforeEach(() => {
+  car = new Car();
+});
+
+describe("Car", () => {
+  it("can park", () => {
+    assert.equal(car.park(), "stopped");
+  });
+
+  it("can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+});
